f0f4f9e54b0d4c5e3791f9a83cc9d720198a5381
leaguecn <leegn366@163.com>
2021-04-15 16:25:32 +0800

    remove old config files

--- bk2hexo.sh
+++
@@ -1,1 +0,0 @@
-mv scaffolds source themes README.md _config.yml package-lock.json package.json ../leaguecn.github.io/ && cd ../leaguecn.github.io/
--- source/categories/index.md
+++
@@ -1,5 +0,0 @@
----
-title: 分类
-date: 2019-02-21 05:37:58
-type: "categories"
----
